c50e68dc3be8dccbccf50880a8ab5fe851e173f2
Vernon Howerton <vhowerton@student.neumont.edu>
2020-02-19 19:09:36 -0700

    Added Models:    Feature, Inventory, Item, SaveThrows, Wallet, Weapon

---
+++ RPGCharacterManager/RPGCharacterManager/Models/Character/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPGCharacterManager.Models.Character
+{
+    public class Feature
+    {
+        public string featureName { get; set; }
+        public string featureDescription { get; set; }
+    }
+}
---
+++ RPGCharacterManager/RPGCharacterManager/Models/Character/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPGCharacterManager.Models.Character
+{
+    public class Item
+    {
+        public string itemName { get; set; }
+        public string itemDescription { get; set; }
+        public uint itemAmount { get; set; }
+
+    }
+}
