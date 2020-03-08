0adcbe5d68e43397e05422a90979a1566f4d8b20
muhahaha100 <oscarbaso@gmail.com>
2020-03-08 16:59:12 -0600

    Add Id and [Key] on some classes

--- RPGCharacterManager/RPGCharacterManager/Models/Character/DeathSaves.cs
+++ RPGCharacterManager/RPGCharacterManager/Models/Character/DeathSaves.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace RPGCharacterManager.Models.Character
 {
     public class DeathSaves
     {
+        [Key]
+        public int Id {get;set;}
         public bool[] SuccessfulSaves { get; set; } = new bool[3];
         public bool[] FailedSaves { get; set; } = new bool[3];
 
